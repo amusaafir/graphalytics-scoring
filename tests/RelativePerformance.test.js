e65c27fe3736c593c9d7709dea775819a211ec3b
AJ <ahmedmusaafir@gmail.com>
2018-03-07 14:30:50 +0100

    Ignore example-results directory; add test setup & test removeUnknownValues function.

---
+++ tests/RelativePerformance.test.js
@@ -0,0 +1,15 @@
+const RelativePerformance = require('../src/competition/scoring/RelativePerformance');
+const UnknownValue = require('../src/benchmark/UnknownValue');
+const expect = require('expect');
+
+it('should remove unknown values - removeUnknownValues()', () => {
+    // Arrange.
+    let scoring = new RelativePerformance();
+    let values = [1, UnknownValue, 4, 3, 8];
+
+    // Act.
+    let removedUnknownvalues = scoring.removeUknownValues(values);
+
+    // Assert.
+    expect(removedUnknownvalues.sort()).toEqual([1, 3, 4, 8]);
+});
